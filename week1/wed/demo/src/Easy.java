99505a6bc3e11bec9c24f275eebcda7192aa1239
jjoyama <jjoyama@gmail.com>
2022-03-22 13:34:37 -0700

    Assignments started 1

---
+++ week1/wed/demo/src/Easy.java
@@ -0,0 +1,11 @@
+package week1.wed.demo.src;
+
+public class Easy {
+    public static int a;
+    public static int b;
+    public static int sum;
+    public static int getSum() {
+        sum = a + b;
+        return sum;
+    }
+}
--- week1/wed/demo/src/Main.java
+++ week1/wed/demo/src/Main.java
@@ -1,11 +1,17 @@
+package week1.wed.demo.src;
+import java.util.Scanner;
+
 public class Main {
+    static Easy e = new Easy();
+    Scanner s1 = new Scanner(System.in);
     int n = 100;
     char c = 's';
     String s = "appian";
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("My name is Jonathan Oyama.");
+        System.out.println("Enter the first number: ");
+
+        e.getSum();
     }
 }
