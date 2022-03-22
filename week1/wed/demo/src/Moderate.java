869bda35b8cc98d356d153855f7ba58ca22ecf92
Russell W Anderson <92950114+Randerson723@users.noreply.github.com>
2022-03-22 14:03:37 -0500

    Tuesday Homework

---
+++ week1/wed/demo/src/Moderate.java
@@ -0,0 +1,13 @@
+public class Moderate extends Main{
+    static boolean problem2(int x) {
+        if(x % 2 == 0) {
+            System.out.println("x is even");
+        } else {
+            System.out.println("x is odd");
+        }
+        return false;
+    }
+
+
+
+}
