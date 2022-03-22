24eb5b33a015292de9509c69639a2fd15df49e63
Gcohen642 <102068961+Gcohen642@users.noreply.github.com>
2022-03-22 15:29:07 -0400

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    String color;
+    String make;
+    String model;
+
+    public static void main(String[] args) {
+        System.out.println("red");
+        System.out.println("jeep");
+        }
+
+}
---
+++ Dog.java
@@ -0,0 +1,19 @@
+public class Dog {
+    String breed;
+    int age;
+    String height;
+
+
+    public void drool () {
+        System.out.println("the dog is drooling");
+    }
+    public void bark () {
+        System.out.println("bark bark");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("bark bark");
+    }
+
+    }
+
