76d55546f235c6f3e62f4784aecbf712029764e5
jjoyama <jjoyama@gmail.com>
2022-03-22 10:07:30 -0700

    jonSnap02

--- week1/wed/demo/src/Main.java
+++ week1/wed/demo/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+    int n = 100;
+    char c = 's';
+    String s = "appian";
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
