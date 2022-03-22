6889a32a86415db9e4b524c46a5bdaf1508af221
ftorrezao <ftorrezao@gmail.com>
2022-03-22 14:19:00 -0400

    FirstProgram

---
+++ Java/src/Week1/tues/Main.java
@@ -0,0 +1,15 @@
+package Week1.tues;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = "Hi everyone, my name is Frank";
+		String b = " and I love Java";
+		
+		System.out.println(a + b);
+
+	}
+
+}
