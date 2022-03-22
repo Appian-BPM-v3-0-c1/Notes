1f35ac6482d8897f51e1d7fe210c05978bf93797
Xavier Davis GitHub <xdd3220@gmail.com>
2022-03-22 14:20:08 -0400

    MY BIO CODE

---
+++ week1/wed/Demo/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I am Xavier Davis. My age is:");
+        System.out.println(30);
+        System.out.println("I want to become a Software Engineer because I am interested in working remotely, and the pay.");
+
+        String s = "I am excited for this journey.";
+        System.out.println(s);
+
+        String c = "How many weeks is the training?";
+        System.out.println(c);
+
+        int n = 12;
+        System.out.println(n);
+    }
+}
