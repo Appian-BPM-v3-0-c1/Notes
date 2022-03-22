6aadca94a0fca293c55428f111e8ea2ee03e0b64
cbaker67 <40648122+cbaker67@users.noreply.github.com>
2022-03-22 14:20:59 -0500

    Add files via upload
    Resolved Problem 2

---
+++ week1/wed/Problem2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        //create scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        //read integer
+        int a = scanner.nextInt();
+
+        //check if number is even or odd
+        ifa%2==0) {
+            System.out.println("The number is even");
+    }        else {
+            System.out.println("The number is odd");
+}
+
