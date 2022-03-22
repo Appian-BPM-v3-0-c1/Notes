cf8b93d5e19f07a95525c8f4331db532e9ec537d
ftorrezao <ftorrezao@gmail.com>
2022-03-22 17:47:51 -0400

    Assigment1

---
+++ Java/src/Week1/tues/Assignment.java
@@ -0,0 +1,20 @@
+package Week1.tues;
+
+import java.nio.file.FileSystemNotFoundException;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 10;
+		int sum = a+ b;
+		
+		System.out.println("Your result is: " + (sum));
+		
+		
+
+	}
+
+}
