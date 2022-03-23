e7316ef15611fe1d892751d5af18d24441e0bebd
CeruleanArc <joan@joaneburgos.com>
2022-03-23 13:13:08 -0600

    Completed Thursday (but really Wednesday) assignment.

--- week1/thu/solutions/src/RPS.java
+++ week1/thu/solutions/src/RPS.java
@@ -2,18 +2,68 @@ import java.util.Scanner;
 
 public class RPS {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String r = '';
+    public void startGame() {
 
-        System.out.println("Enter first number: ");
-        a = input.nextInt();
+        char user = ' ';
+        int userwins = 0;
+        int cpuwins = 0;
+        int draws = 0;
 
-        System.out.println("Enter second number: ");
-        b = input.nextInt();
+        System.out.println("Welcome to RPS");
 
-        sum = a + b;
+        while (user != 'x') {
 
-        System.out.println("The sum of the two numbers is: "+sum);
+            int cpu = getRandomNumber();
+            int round = 0;
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("\nPick \"r\" (rock), \"p\" (paper), \"s\" (scissors), or \"x\" (exit).");
+
+            user = scan.nextLine().charAt(0);
+
+            if (user == 'r' && cpu == 0) {
+                System.out.println("DRAW!");
+                draws = draws + 1;
+            } else if (user == 'r' && cpu == 1) {
+                System.out.println("CPU wins!");
+                cpuwins = cpuwins + 1;
+            } else if (user == 'r' && cpu == 2) {
+                System.out.println("PLAYER wins!");
+                userwins = userwins + 1;
+            } else if (user == 's' && cpu == 0) {
+                System.out.println("CPU wins!");
+                cpuwins = cpuwins + 1;
+            } else if (user == 's' && cpu == 1) {
+                System.out.println("PLAYER wins!");
+                userwins = userwins + 1;
+            } else if (user == 's' && cpu == 2) {
+                System.out.println("DRAW!");
+                draws = draws + 1;
+            } else if (user == 'p' && cpu == 0) {
+                System.out.println("PLAYER wins!");
+                userwins = userwins + 1;
+            } else if (user == 'p' && cpu == 1) {
+                System.out.println("CPU wins!");
+                cpuwins = cpuwins + 1;
+            } else if (user == 'p' && cpu == 2) {
+                System.out.println("DRAW!");
+                draws = draws + 1;
+            } else if (user == 'x') {
+                System.out.println("Thank you for playing!");
+            } else {
+                System.out.println("You have chosen poorly.");
+            }
+
+        }
+
+        System.out.println("\n\033[4;2m" + "FINAL SCORES" + "\033[0m");
+        System.out.println("\nTotal Player wins: " + userwins);
+        System.out.println("Total CPU wins: " + cpuwins);
+        System.out.println("Total Draws: " + draws);
+
+    }
+
+    public int getRandomNumber() {
+        return (int) (Math.random() * 3);
     }
 }
