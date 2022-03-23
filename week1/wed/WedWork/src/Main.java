45cbc0a0fd0e776ed45554eeeed710b90f881bc8
Russell W Anderson <92950114+Randerson723@users.noreply.github.com>
2022-03-23 11:04:58 -0500

    refactored work

---
+++ week1/wed/WedWork/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Problems problems = new Problems();
+
+        System.out.println(problems.easy(3,4));
+        System.out.println(problems.moderate(42));
+        System.out.println(problems.hard("Generalization"));
+    }
+}
