4b2ef00efe0914abceca2b94508347679af794b2
ftorrezao <ftorrezao@gmail.com>
2022-03-23 13:28:07 -0400

    update fix 03/23/22 1:28pm

--- Java/Java/src/Week1/wed/Main.java
+++ Java/Java/src/Week1/wed/Main.java
@@ -7,7 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-				
+		
+		
 		Problem1 prb = new Problem1();
 		
 		System.out.println(prb.easy(5, 3));
