c261ee82ce1f03a38ba6692dfe3b6a5636868f1e
Russell W Anderson <92950114+Randerson723@users.noreply.github.com>
2022-03-23 09:33:29 -0500

    assignment

---
+++ week1/wed/work/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    static int problem1(
+            int a,
+            int b
+    ) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+
+    }
+}
---
+++ week1/wed/work/src/Moderate.java
@@ -0,0 +1,16 @@
+public class Moderate {
+    public static void moderate(String[] args) {
+        int x = 20;
+        if (x % 2 == 0) {
+            System.out.println("x is even");
+        } else {
+            System.out.println("x is odd");
+        }
+
+    }
+}
+
+
+
+
+
