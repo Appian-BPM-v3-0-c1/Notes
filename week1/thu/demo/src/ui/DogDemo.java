66cefb807f707be710b51290cb92a8fbd12ab673
bduong0929 <bao.duong@revature.com>
2022-03-24 10:23:35 -0600

    added arrDemo and dogDemo to week1/thu

---
+++ week1/thu/demo/src/ui/DogDemo.java
@@ -0,0 +1,15 @@
+package ui;
+
+import models.Dog;
+
+public class DogDemo {
+    public void start() {
+        Dog myNewDog = new Dog();
+
+        /* Create a dog class with private attributes name, breed, color, size, and hair */
+        /* Create behavior of you dog */
+        /* Create constructors for the dog class */
+        /* generate toString() method */
+        /* generate getters/setters for your dog class */
+    }
+}
