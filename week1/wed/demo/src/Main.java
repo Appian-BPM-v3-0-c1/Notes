980319594648add52cc6c6d19cdd948d551e3e8c
AurumCrystallo <jacob240@revature.net>
2022-03-24 10:19:36 -0400

    Everything is good

--- week1/wed/demo/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Problems problems = new Problems();
-        System.out.println(problems.easy());
-        System.out.println(problems.moderate());
-        System.out.println(problems.finalBoss());
-    }
-}
