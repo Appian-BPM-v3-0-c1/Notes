71db8db96b7d4b85a9986a2da7bf97a63d4c764c
ftorrezao <ftorrezao@gmail.com>
2022-03-24 14:46:55 -0400

    Update lesson loops and arrays 03/24/22    2:46pm

--- Java/Java/src/Week1/thur/Dog.java
+++ Java/Java/src/Week1/thur/Dog.java
@@ -1,5 +1,91 @@
 package Week1.thur;
 
 public class Dog {
+	
+	private String name;
+	private String breed;
+	private String color;
+	private String size;
+	private String hair;
+	
+	
+	
+	public Dog() 
+	 {
+		 
+	 }
+
+	
+	public Dog(String name, String breed, String color, String size, String hair )
+	{
+		this.name = name;
+		this.breed = breed;
+		this.color = color;
+		this.size = size;
+		this.hair = hair;
+		
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + ", breed=" + breed + ", color=" + color + ", size=" + size + ", hair=" + hair
+				+ "]";
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getBreed() {
+		return breed;
+	}
+
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+
+	public String getColor() {
+		return color;
+	}
+
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+
+	public String getSize() {
+		return size;
+	}
+
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+
+	public String getHair() {
+		return hair;
+	}
+
+
+	public void setHair(String hair) {
+		this.hair = hair;
+	}
+
+
+	
 
 }
+
+ 
---
+++ Java/Java/src/Week1/thur/Main.java
@@ -0,0 +1,15 @@
+package Week1.thur;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Dog myDog = new Dog("brownie","mix", "brown", "large", "short");
+		
+		System.out.println(myDog);
+
+	}
+
+}
