e73a72f1619bf51a79315963f4f11a1636a82d09
David Mace <david12mace@gmail.com>
2022-03-25 10:24:15 -0400

    Update again 3/25/2022 10:24 AM

--- week1/wed/Demo/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int n = 100;
-
-        System.out.println(n);
-        System.out.println("I have a computer science degree");
-
-    }
-}
