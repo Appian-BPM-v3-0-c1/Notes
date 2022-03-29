262140f7db685a42cadb5bcaf0e8b0b35da7de29
drmdeveloper1 <101901402+drmdeveloper1@users.noreply.github.com>
2022-03-29 14:49:58 -0500

    Create Shape.java

---
+++ week2/tue/Shape.java
@@ -0,0 +1,15 @@
+package Main;
+
+public abstract class Shape {
+
+    public Shape() {
+
+        int height;
+        int length;
+        int Area;
+
+        return;
+
+        Area = (length * width);
+        }
+    }
