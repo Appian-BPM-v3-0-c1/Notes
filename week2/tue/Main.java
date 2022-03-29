657bbe71dc81ca0fee007c4ef1754a245f93ab04
drmdeveloper1 <101901402+drmdeveloper1@users.noreply.github.com>
2022-03-29 14:48:56 -0500

    Create Main.java
    Framework

---
+++ week2/tue/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class Main {
+
+    public static void main (String[] args) {
+        Area = new Area();
+        length = new length();
+        width = new width();
+    }
+}
