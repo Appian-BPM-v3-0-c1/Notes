bafdebbefec79c8e0f087bc58fa5087cecf2f22f
drmdeveloper1 <101901402+drmdeveloper1@users.noreply.github.com>
2022-03-30 09:28:09 -0500

    Update Main.java

--- week2/tue/Main.java
+++ week2/tue/Main.java
@@ -1,10 +1,6 @@
-package Main;
-
 public class Main {
 
     public static void main (String[] args) {
-        Area = new Area();
-        length = new length();
-        width = new width();
+       
     }
 }
