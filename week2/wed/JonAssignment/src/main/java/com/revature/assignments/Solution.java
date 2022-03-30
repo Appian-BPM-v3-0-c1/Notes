66fd7cb8ced3eb519c68acecf53ab924e54a8b50
jjoyama <jjoyama@gmail.com>
2022-03-30 15:12:40 -0700

    completed part one of Bao's coding challenge

--- week2/wed/JonAssignment/src/main/java/com/revature/assignments/Solution.java
+++ week2/wed/JonAssignment/src/main/java/com/revature/assignments/Solution.java
@@ -1,7 +1,32 @@
 package com.revature.assignments;
+import java.util.Arrays;
 
 public class Solution {
-
     public Solution() {}
+    public int[] twoSum(int[] nums, int target) {
+        int i;
+        int j;
+        int count = 0;
+        int[] arr = new int[0];
 
+        for (i=0; i < nums.length; i++) {
+            for (j=i + 1;j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    arr = Arrays.copyOf(arr, arr.length + 1);
+                    arr[arr.length - 1] = i;
+                    count++;
+                    arr = Arrays.copyOf(arr, arr.length + 1);
+                    arr[arr.length - 1] = j;
+                    count++;
+                }
+            }
+        }
+        if (count > 2){
+            Arrays.sort(arr);
+        }
+        return arr;
+    }
+    public boolean isPalindrome(int x) {
+        throw new RuntimeException();
+    }
 }
---
+++ week2/wed/JonAssignment/src/test/java/com/revature/assignments/SolutionTest.java
@@ -0,0 +1,35 @@
+package com.revature.assignments;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    Solution solution = new Solution();
+
+    @Test
+    public void twoSum() {
+        int[] arr1 = {2, 7, 11, 15};
+        int t1 = 9;
+        Assert.assertArrayEquals(new int[]{0,1}, solution.twoSum(arr1, t1));
+
+        int[] arr2 = {3,2,4};
+        int t2 = 6;
+        Assert.assertArrayEquals(new int[]{1,2}, solution.twoSum(arr2, t2));
+
+        int[] arr3 = {3,3};
+        int t3 = 6;
+
+        Assert.assertArrayEquals(new int[]{0,1},solution.twoSum(arr3,t3));
+
+        int[] arr4 = {-5, -1, 0, 0, 5, 1};
+        int t4 = 0;
+
+        Assert.assertArrayEquals(new int[]{0,1,2,3,4,5}, solution.twoSum(arr4,t4));
+    }
+
+    @Test
+    public void isPalindrome() {
+    }
+}
