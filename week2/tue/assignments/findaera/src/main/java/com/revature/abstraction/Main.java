4ae93f516e3e88d3a2c15efcc2109c3f4870cca4
Sweta <sweta.prasad.01@gmail.com>
2022-03-30 13:57:37 -0400

    abstraction assignment

---
+++ week2/tue/assignments/findaera/src/main/java/com/revature/abstraction/Main.java
@@ -0,0 +1,16 @@
+package com.revature.abstraction;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+
+//* Part 1
+// com.revature.abstraction.Shape:calcuate Aera() and calculate Circumtance() no parameters and return a double
+//Part 2
+ // Circle class his class will have a constructor, accessors and mutators, and the abstract methods defined for this class.
+   //Re
+}
---
+++ week2/tue/assignments/findaera/src/main/java/com/revature/abstraction/Rectangle.java
@@ -0,0 +1,4 @@
+package com.revature.abstraction;
+
+public class Rectangle {
+}
