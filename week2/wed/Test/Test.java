9f7a30565cc51ee911c69a1f172270f2ef226b09
drmdeveloper1 <101901402+drmdeveloper1@users.noreply.github.com>
2022-03-31 06:54:37 -0500

    Create Test.java

---
+++ week2/wed/Test/Test.java
@@ -0,0 +1,42 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+  Solution solution = new Solution();
+  
+  @Test
+  public void twoSum() {
+    int[] arr1 = {2, 7, 11, 15};
+    int t1 = 9;
+    
+    Assert.assertEquals(new int[]{0, 1}, solution.twoSum(arr1, t1));
+    
+    int[] arr2 = {3, 2, 4};
+    int t2 = 6;
+    
+    Assert.assertEquals(new int[]{1, 2}, solution.twoSum(arr2, t2));
+    
+    int[] arr3 = {3, 3};
+    int t3 = 6;
+    
+    Assert.assertEquals(new int[]{0, 1}, solution.twoSum(arr3, t3));
+    
+    int[] arr4 = {-5, -1, 0, 0, 5, 1};
+    int t4 = 0;
+    
+    Assert.assertEquals(new int[]{0, 1, 2, 3, 4, 5}, solution.twoSum(arr4, t4));
+    
+  }
+  
+  @Test
+  public void isPalindrome() {
+    Assert.assertEquals(true, solution.isPalindrome(121));
+    Assert.assertEquals(true, solution.isPalindrome(-123));
+    Assert.assertEquals(true, solution.isPalindrome(123));
+    Assert.assertEquals(true, solution.isPalindrome(101));
+    Assert.assertEquals(true, solution.isPalindrome(10));
+    Assert.assertEquals(true, solution.isPalindrome(0));
+  }
+}
