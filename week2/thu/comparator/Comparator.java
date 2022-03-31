49de7dfee5a736d1f276fa48ec99ac2b37602807
drmdeveloper1 <101901402+drmdeveloper1@users.noreply.github.com>
2022-03-31 12:42:13 -0500

    Create Comparator.java

---
+++ week2/thu/comparator/Comparator.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Employee {
+    int EmployeeID;
+    String name, address;
+    public Employee int EmployeeID, String name, String
+                    this.EmployeeID = EmployeeID;
+                    this.name = name;
+                    this.address = address;
+}
+public String toString() {
+    return this.EmployeeID + "" + this.name
+    }
+}
+class Sortbyname implements Comparator<Employee>
+        public int compare(Employee a, Employee return a.name.compareTo(b.name);
+        }
+    }
