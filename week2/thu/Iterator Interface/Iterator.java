3e197498a0f19c9f269d71ca9c94bf081d9dc09c
drmdeveloper1 <101901402+drmdeveloper1@users.noreply.github.com>
2022-03-31 12:52:13 -0500

    Update Iterator.java

--- week2/thu/Iterator Interface/Iterator.java
+++ week2/thu/Iterator Interface/Iterator.java
@@ -2,47 +2,21 @@ import java.util.*;
 import java.io.*;
 class Employees implements Iterable {
   
-  List<String> str = null;
+  List<String> string = null;
   public Employees()
   {
-    str = new ArrayList<String>();
-    str.add("Customer");
-    str.add("Customer1");
-    str.add("Customer2");
-    str.add("Customer3");
-    str.add("Customer4");
-    str.add("Customer5");
-    str.add("Customer6");
+    string = new ArrayList<String>();
+    string.add("Customer");
+    string.add("Customer1");
+    string.add("Customer2");
+    string.add("Customer3");
+    string.add("Customer4");
+    string.add("Customer5");
+    string.add("Customer6");
   }
   
   @Override public Iterator<String> iterator()
   {
-    return str.iterator();
+    return string.iterator();
     }
 }
-
-
-public class Myclass {
-  public static void main(String[] args)
-  {
-    ArrayList<Integer> numbers
-      = new ArrayList<Integer>();
-    numbers.add(1);
-    numbers.add(2);
-    numbers.add(3);
-    numbers.add(4);
-    
-    Iterator<Integer> it = numbers.iterator();
-    while (it.hasNext()) {
-      
-      Integer i = it.next();
-      
-      if(i < 10) {
-        
-        it.remove();
-      }
-    }
-    System.out.println(numbers);
-    }
-}
-  
