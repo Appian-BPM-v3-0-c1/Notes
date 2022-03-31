c2fa65567633e5616a0717a91e850bf215181fa8
TheLettersXAI <xdd3220@gmail.com>
2022-03-31 11:37:34 -0400

    Pushing to Pull

--- week1/wed/Solutions/src/Main.java
+++ week1/wed/Solutions/src/Main.java
@@ -3,17 +3,21 @@ public class Main {
     public static <string> void main(String[] args) {
         Problems problems = new Problems();
         Scanner input = new Scanner(System.in);
+
+        System.out.println("EASY:");
         System.out.println("Enter the first number: ");
         int a = input.nextInt();
         System.out.println("Enter the second number: ");
         int b = input.nextInt();
         System.out.println("The sum of these 2 numbers is: " + problems.easy(a,b));
 
-        System.out.println("\nEnter a number to determine whether it is even or odd: ");
+        System.out.println("\nMODERATE:");
+        System.out.println("Enter a number to determine whether it is even or odd: ");
         a = input.nextInt();
         System.out.println(problems.moderate(a));
 
-        System.out.println("\nEnter a word: ");
+        System.out.println("\nHARD:");
+        System.out.println("Enter a word: ");
         String s = input.next();
         System.out.println(problems.hard(s));
 
